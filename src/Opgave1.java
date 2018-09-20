216a5eb4aa45a09df49d807371595048a31fa5f0
MathiasAmi <42993917+MathiasAmi@users.noreply.github.com>
2018-09-20 11:26:57 +0200

    Update Opgave1.java

--- src/Opgave1.java
+++ src/Opgave1.java
@@ -4,5 +4,53 @@ public class Opgave1 {
         System.out.println("Welcome to Java");
         System.out.println("Learning Java now");
         System.out.println("Programming is fun");
-    }
-}
+
+
+//Opgave 1.2
+        System.out.println("");
+        System.out.println("Opgave 1.2");
+
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+
+//Opgave 1.3
+        System.out.println("");
+        System.out.println("Opgave 1.3");
+
+        System.out.println("    J");
+        System.out.println("J  aaa   v    vaaa");
+        System.out.println("J     aa     v  v   a  a");
+        System.out.println("J   aaaa       v     aaaa");
+
+//Opgave 1.4
+        System.out.println("");
+        System.out.println("Opgave 1.4");
+
+        System.out.println("a   a^2   a^3   a^4");
+        System.out.println("1    1     1      1");
+        System.out.println("2    4     8      16");
+        System.out.println("3    9     27     81");
+        System.out.println("4    16    64     256");
+
+//Opgave 1.5
+        System.out.println("");
+        System.out.println("Opgave 1.5");
+
+        // Bruger ikke anførselstegn her, da den skal regne et stykke, og ikke skrive en tekst.
+
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
+
+//Opgave 1.6
+        System.out.println("");
+        System.out.println("Opgave 1.6");
+
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7+ 8 + 9 + 10);
+
+//Opgave 1.7
+        System.out.println("");
+        System.out.println("Opgave 1.7");
+
+        
